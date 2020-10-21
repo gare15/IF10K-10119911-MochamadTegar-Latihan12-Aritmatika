923e0813fc2a4333a8f90e977a5dff064ba83221
gare15 <72866876+gare15@users.noreply.github.com>
2020-10-21 09:01:43 +0700

    Aritmatika

---
+++ src/pbo/pkg10119911/latihan12/aritmatika/Aritmatika.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo.pkg10119911.latihan12.aritmatika;
+/**
+ *
+ * @author 
+ * NAMA     : Mochamad Tegar Dwi S
+ * KELAS    : IF10K
+ * NIM      : 10119911 
+ * Deskripsi Program : Aritmatika
+ */
+public class Aritmatika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    
+          int a = 10;
+          int b = 20;
+          int c = 25;
+          int d = 25;
+          System.out.println("a + b = " + (a + b));
+          System.out.println("a - b = " + (a - b));
+          System.out.println("a * b = " + (a * b));
+          System.out.println("b / a = " + (b / a));
+          System.out.println("b % a = " + (b % a));
+          System.out.println("c % a = " + (c % a));
+          System.out.println("a++ = " + (a++));
+          System.out.println("b-- = " + (b--));
+          System.out.println("d++ = " + (d++));
+          System.out.println("++d = " + (++d));
+          
+          
+    }
+    
+}
